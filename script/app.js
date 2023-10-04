9038f2e3dbe0af08a4795b396cc9032740fdbcfd
jentlAntheunis <113892951+jentlAntheunis@users.noreply.github.com>
2023-10-04 15:48:37 +0200

    omg look at that little wave how cuteee

--- script/app.js
+++ script/app.js
@@ -1,21 +1,55 @@
 const init = () => {
-    let tl = gsap.Timeline({
-        defaults: {
-            duration: .1,
-            ease: 'power1.inOut',
-        },
-    });
+  let hover = gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: 'power1.inOut',
+      repeat: Infinity,
+      yoyo: true,
+    },
+  });
 
-    tl.fromTo('#Robot',{
-        y: 2.5,
-      },{
-        y: -2.5,
-      })
-      .to('#Shadow', {
-        scale: 0.75,
-      },
-        '-=.75',
-      )
+  hover.set('#Shadow', { transformOrigin: '50% 100%' })
+    .to('#Robot', { y: -2.5, })
+    .to('#Shadow', {
+      scale: 0.75,
+    },
+    {
+      scale: 1,
+    }
+  );
+
+  let head = gsap.timeline({
+    defaults: {
+      duration: .8,
+      ease: 'power1.inOut',
+      repeat: Infinity,
+      yoyo: true,
+    },
+  });
+
+  head.set('#Head', { transformOrigin: '50% 100%', })
+    .to('#Head', {
+      rotate: 2,
+    },
+  )
+
+  let armWave = gsap.timeline({
+    defaults: {
+      duration: .3,
+      ease: 'power1.inOut',
+      repeat: Infinity,
+      yoyo: true,
+    },
+  });
+
+  armWave.set('#Arm-left', {
+    transformOrigin: '100% 0%'
+  })
+  .set('#Arm-right', {
+    transformOrigin: '0% 50%'
+  })
+  .to('#Arm-left', { rotate: 1 })
+  .to('#Arm-right', { rotate: -15 }, '<');
 
 };
 
